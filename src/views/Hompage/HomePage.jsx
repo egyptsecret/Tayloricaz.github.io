eae24c1b94606b8dc9652f41dce4a697c5283901
u9147298 <u9147298@bsmch.net>
2023-07-18 14:21:11 +0300

    bug

--- src/views/Hompage/HomePage.jsx
+++ src/views/Hompage/HomePage.jsx
@@ -16,17 +16,19 @@ export const HomePage = () => {
   return (
     <div className={classes.allErasImg}>
       <SiteBanner />
-      <button className="px-8 pop-shadow py-2 text-orange-100 whitespace-nowrap font-mono rounded-full bg-violet-300 hover:bg-violet-400 active:bg-violet-500 focus:outline-none focus:ring focus:ring-violet-100 ">
-        take me to a random song quiz
-      </button>
-      <AlbumsOptions numOfSongs={arrayOfSongs.length} />
       <Link
         to={`/songquiz`}
         state={{
           songNum: getRandomInt(arrayOfSongs.length),
           numOfSongs: arrayOfSongs.length,
         }}
-      ></Link>
+      >
+      <button className="px-8 pop-shadow py-2 text-orange-100 whitespace-nowrap font-mono rounded-full bg-violet-300 hover:bg-violet-400 active:bg-violet-500 focus:outline-none focus:ring focus:ring-violet-100 ">
+        take me to a random song quiz
+      </button>
+      </Link>
+      <AlbumsOptions numOfSongs={arrayOfSongs.length} />
+
     </div>
   );
 };
